c404569fed572a81061f1ac7a286bd98757c4012
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2024-12-11 17:53:05 +0100

    Figma Component

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,42 +1,43 @@
 import 'package:figflow/figflow.dart';
 import 'package:flutter/material.dart';
-import 'package:figma/figma.dart' as figma_api;
 
 void main() async {
   const token = String.fromEnvironment("TOKEN");
   const fileKey = String.fromEnvironment("FILE_KEY");
 
-  final client = figma_api.FigmaClient(token);
+  await FigmaService.instance.initialize(
+    accessToken: token,
+    fileId: fileKey,
+  );
 
-  final document = await client.getFile(fileKey);
-
-  final root = document.document!;
-
-  runApp(FigmaTestApp(
-    node: root,
-  ));
+  runApp(const FigmaTestApp());
 }
 
 class FigmaTestApp extends StatelessWidget {
   const FigmaTestApp({
-    required this.node,
     super.key,
   });
-
-  final figma_api.Node node;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF222222),
         appBar: AppBar(
           title: const Text(
             'Figma Layout + Rectangle Tests',
           ),
         ),
-        body: FigmaConverter().convertNode(node),
+        body: const Center(
+          child: TextExample(),
+        ),
       ),
     );
   }
+}
+
+class TextExample extends FigmaComponent {
+  const TextExample({super.key});
+
+  @override
+  String get figmaComponentId => "text_example";
 }
